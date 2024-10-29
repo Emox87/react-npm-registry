b3a285ea8d8f74e1075d0c13bc554bdd814018e0
Emil Tsvetanov <tsvetanovemil87@gmail.com>
2024-10-29 20:56:54 +0200

    Adding Pages and RootLayout

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,33 @@
-import "./App.css";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
+import RootLayout from "./layouts/RootLayout";
+import HomePage from "./pages/HomePage";
+import SearchPage from "./pages/SearchPage";
+import DetailsPage from "./pages/DetailsPage";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <RootLayout />,
+    children: [
+      {
+        index: true,
+        element: <HomePage />,
+      },
+      {
+        path: "/search",
+        element: <SearchPage />,
+      },
+      {
+        path: "/packages/:name",
+        element: <DetailsPage />,
+      },
+    ],
+  },
+]);
 
 function App() {
-  return (
-    <>
-      <h1 className="text-3xl font-bold underline">Hello world!</h1>
-    </>
-  );
+  return <RouterProvider router={router} />;
 }
 
 export default App;
